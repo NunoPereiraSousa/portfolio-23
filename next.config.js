ac8e6b1d47677aab2d3180c610b4772808aba943
NunoPereiraSousa <nunopereirasousa00@gmail.com>
2023-11-17 12:05:11 +0000

    Changed project config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  distDir: "dist",
-  swcMinify: true,
-  unoptimized: true,
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
